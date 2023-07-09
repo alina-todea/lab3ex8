ef247902afe02dd755e85cedcbbd172f73183ba0
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-09 10:44:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab3ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*Scrieti o functie recursiva care pentru un numar “n” primit ca parametru, 
+             va afisa urmatoarea piramida a numerelor:
+             */
+
+            Console.WriteLine("Introduceti un numar:");
+            int n = int.Parse(Console.ReadLine());
+
+            AfisarePiramida(4);
+
+        }
+
+        static void AfisarePiramida(int n)
+        {
+
+            if (n<=0)
+            {
+                return;
+            }
+
+            
+            
+            for (int i = 0; i <= n - 1; i++)
+
+            {
+                Console.Write(n);
+
+            }
+            Console.WriteLine();
+
+            AfisarePiramida(n - 1);
+
+
+
+        }
+    }
+}
